efbe1a9b01e9937f1684a61952bfe4798bd2befa
thotasrinath <srinath.thota@outlook.com>
2023-02-23 15:17:36 +0800

    Stable tests

--- data-gen-for-read-perf.js
+++ data-gen-for-read-perf.js
@@ -8,6 +8,14 @@ export default () => {
     count++;
 }
 
+function randomDate(start, end, startHour, endHour) {
+    var date = new Date(+start + Math.random() * (end - start));
+    var hour = startHour + Math.random() * (endHour - startHour) | 0;
+    date.setHours(hour);
+    return date;
+}
+
+
 function getRecord(id) {
     return {
         "docI1d": `${id}`,
@@ -1853,9 +1861,3 @@ function getRecord(id) {
     };
 }
 
-function randomDate(start, end, startHour, endHour) {
-    var date = new Date(+start + Math.random() * (end - start));
-    var hour = startHour + Math.random() * (endHour - startHour) | 0;
-    date.setHours(hour);
-    return date;
-}
